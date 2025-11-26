564275032134fe6ea466b36ab162708e89c6cae1
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2025-11-26 16:19:05 +0000

    feat(core): create core package
    Create models and enums

---
+++ src/main/java/com/java_tbt/api/core/models/Address.java
@@ -0,0 +1,52 @@
+package com.java_tbt.api.core.models;
+
+import com.java_tbt.api.application.dto.address.AddressDTOCreate;
+import com.java_tbt.api.application.dto.address.AddressDTOUpdate;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    private String firstLine;
+    private String secondLine;
+    private int number;
+    private String zipCode;
+    private String complement;
+
+    public Address(AddressDTOCreate address) {
+        firstLine = address.firstLine();
+        secondLine = address.secondLine();
+        number = address.number();
+        zipCode = address.zipCode();
+        complement = address.complement();
+    }
+
+    public Address update(AddressDTOUpdate address) {
+        if (address.firstLine() != null) {
+            firstLine = address.firstLine();
+        }
+        if (address.secondLine() != null) {
+            secondLine = address.secondLine();
+        }
+        if (address.number().isPresent()) {
+            number = address.number().getAsInt();
+        }
+        if (address.zipCode() != null) {
+            zipCode = address.zipCode();
+        }
+        if (address.complement() != null) {
+            complement = address.complement();
+        }
+        return this;
+    }
+
+}
---
+++ src/main/java/com/java_tbt/api/core/models/Doctor.java
@@ -0,0 +1,79 @@
+package com.java_tbt.api.core.models;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.dialect.type.PostgreSQLEnumJdbcType;
+
+import com.java_tbt.api.application.dto.doctor.DoctorDTOCreate;
+import com.java_tbt.api.application.dto.doctor.DoctorDTOUpdate;
+import com.java_tbt.api.core.enums.Specialties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Doctor")
+@Table(name = "doctors")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    @Column(unique = true)
+    private String crm;
+    private String email;
+    @Enumerated(EnumType.STRING)
+    @JdbcType(value = PostgreSQLEnumJdbcType.class)
+    private Specialties specialty;
+    private String phone;
+    private Boolean active = true;
+    @Embedded
+    private Address address;
+
+    public Doctor(DoctorDTOCreate doctor) {
+        name = doctor.name();
+        crm = doctor.crm();
+        email = doctor.email();
+        specialty = doctor.specialty();
+        phone = doctor.phone();
+        address = new Address(doctor.address());
+    }
+
+    public Doctor update(DoctorDTOUpdate doctor) {
+        if (doctor.name() != null) {
+            name = doctor.name();
+        }
+        if (doctor.email() != null) {
+            email = doctor.email();
+        }
+        if (doctor.phone() != null) {
+            phone = doctor.phone();
+        }
+        if (doctor.address() != null) {
+            address = address.update(doctor.address());
+        }
+        return this;
+    }
+
+    public Doctor delete() {
+        active = false;
+        return this;
+    }
+
+}
