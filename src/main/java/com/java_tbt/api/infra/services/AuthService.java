4a6939873b56ea5af6e2d5f6c83561349e1d5315
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2025-11-26 22:03:42 +0000

    feat(infra): create auth and token services

---
+++ src/main/java/com/java_tbt/api/infra/services/AuthService.java
@@ -0,0 +1,22 @@
+package com.java_tbt.api.infra.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.java_tbt.api.data.repositories.UserRepository;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username);
+    }
+
+}
