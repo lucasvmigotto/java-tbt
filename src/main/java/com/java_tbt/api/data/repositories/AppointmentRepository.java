ca4d4aac9f8c9d56bf6c9bba7642e75f098ffaf4
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2025-11-28 16:09:00 +0000

    feat(data): add appointments list method

--- src/main/java/com/java_tbt/api/data/repositories/AppointmentRepository.java
+++ src/main/java/com/java_tbt/api/data/repositories/AppointmentRepository.java
@@ -3,6 +3,8 @@ package com.java_tbt.api.data.repositories;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.java_tbt.api.core.models.Appointment;
@@ -14,4 +16,18 @@ public interface AppointmentRepository extends JpaRepository<Appointment, UUID> 
             LocalDateTime endPeriod);
 
     boolean existsByDoctorIdAndIsCancelledFalseAndDatetime(UUID idDoctor, LocalDateTime datetime);
+
+    Page<Appointment> findAllByDoctorIdAndIsCancelledAndDatetimeBetween(
+            UUID idDoctor,
+            boolean isCancelled,
+            LocalDateTime startPeriod,
+            LocalDateTime endPeriod,
+            Pageable pagination);
+
+    Page<Appointment> findAllByPatientIdAndIsCancelledAndDatetimeBetween(
+            UUID idPatient,
+            boolean isCancelled,
+            LocalDateTime startPeriod,
+            LocalDateTime endPeriod,
+            Pageable pagination);
 }
